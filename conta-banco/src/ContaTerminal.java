8bc0658d3e9b759230628b292b46f047ed5e46f4
Guilherme-victor98 <guiboy-777@hotmail.com>
2025-05-02 01:24:47 -0300

    Desafio do Projeto DIO

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -14,6 +14,7 @@ public class ContaTerminal {
         sc.nextLine();
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo $" + saldo + " já está disponível para saque.");
         
+        
         sc.close();
     }
 }
